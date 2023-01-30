f67ce37ea74bb2a558c2727685b63d98070dee18
Bot Bahlul <88623122+botbahlul@users.noreply.github.com>
2023-01-31 04:35:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_description = (
 
 setup(
     name="autosrt",
-    version="0.0.6",
+    version="0.0.5",
     description="autosrt is a utility for automatic speech recognition and subtitle generation. It takes a video or an audio file as input, performs voice activity detection to find speech regions,  makes parallel requests to Google Web Speech API  to generate transcriptions for those regions,  (optionally) translates them to a different language, and finally saves the resulting subtitles to disk. It supports a variety of input and output languages  and can currently produce subtitles in SRT format.",
     long_description = long_description,
     author="Bot Bahlul",
